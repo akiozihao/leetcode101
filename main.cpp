89df3c7ac5e874fcc5394c61ce461bb7376eb36a
akio <akiozihaoceat@gmail.com>
2021-02-22 00:39:39 +0800

    daily

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-int main()
-{
-    priority_queue<int, vector<int>,greater<>> small;
-    priority_queue<int> large;
-    large.push(1);
-    large.push(2);
-    small.push(1);
-    small.push(2);
-    cout << small.top() << endl;
-    cout << large.top() << endl;
+int main() {
+  priority_queue<int, vector<int>, greater<>> small;
+  priority_queue<int> large;
+
+  large.push(1);
+  large.push(2);
+  small.push(1);
+  small.push(2);
+  cout << small.top() << endl;
+  cout << large.top() << endl;
 }
