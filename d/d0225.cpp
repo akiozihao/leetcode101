d4e519e9016e51ed054a7d248d8c4437947b2793
akiozihao <akiozihaoceat@gmail.com>
2021-02-25 01:57:42 +0800

    daily

---
+++ d/d0225.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        if (matrix.empty() || matrix[0].empty()) return matrix;
+        int m = matrix.size(), n = matrix[0].size();
+        vector<vector<int>> res(n, vector<int>(m));
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                res[j][i] = matrix[i][j];
+            }
+        }
+        return res;
+    }
+};
