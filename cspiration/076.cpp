5c1e4b2f78fcc0f37b94c06d0507755dbd7a6aa6
akiozihao <akiozihaoceat@gmail.com>
2021-03-03 14:44:33 +0800

    daily

---
+++ cspiration/076.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if (t.empty()) return "";
+        string res;
+        vector<int> letterCnt(256, 0);
+        int left = 0, cnt = 0, minLeft = -1, minLen = INT_MAX;
+        for (char c : t) ++letterCnt[c];
+        for (int i = 0; i < s.size(); ++i) {
+            if (--letterCnt[s[i]] >= 0) ++cnt;
+            while (cnt == t.size()) {
+                if (minLen > i - left + 1) {
+                    minLen = i - left + 1;
+                    minLeft = left;
+                }
+                if (++letterCnt[s[left]] > 0) --cnt;
+                ++left;
+            }
+        }
+        return minLeft == -1 ? "" : s.substr(minLeft, minLen);
+    }
+};
